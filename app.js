a1751269a200619b26731d337f9a0f611d36fc20
thiagoafcf <thiago.afcf@gmail.com>
2024-09-04 18:18:10 -0300

    Criação da base de dados e associação com o app.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Hello, World!");
+console.log(dados[0].descricao);
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo : 'Rayssa Leal',
+        descricao : 'Rayssa Leal, conhecida como "Fadinha", é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos de idade, ela já colecionava títulos e patrocinadores, tornando-se um símbolo da nova geração do skate. Sua participação nos Jogos Olímpicos de Tóquio a projetou para o cenário mundial, inspirando jovens skatistas e quebrando barreiras no esporte.',
+        link : 'https://pt.wikipedia.org/wiki/Rayssa_Leal'
+    },
+    {
+        titulo: 'Rebeca Andrade',
+        descricao: 'Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos. Com seu talento e dedicação, ela se tornou a maior medalhista olímpica do Brasil, conquistando diversas medalhas de ouro, prata e bronze. Sua performance nas Olimpíadas de Tóquio e Paris a consagrou como um ícone do esporte nacional, inspirando milhões de jovens brasileiros a praticarem ginástica e a perseguirem seus sonhos. Além de suas conquistas esportivas, Rebeca também é conhecida por sua simpatia e carisma, sendo um verdadeiro exemplo de superação e determinação.',
+        link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade'
+    },
+    {
+        titulo: 'Beatriz Souza',
+        descricao: 'Beatriz Souza é uma judoca brasileira, campeã olímpica. Conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris, tornando-se um símbolo do judô brasileiro. Sua força e determinação a levaram ao topo do pódio, inspirando novas gerações de judocas.',
+        link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza'
+    }
+];
