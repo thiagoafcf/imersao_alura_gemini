a22fc7039c3a5c672aa0a031c132d1e93470d9f5
thiagoafcf <thiago.afcf@gmail.com>
2024-09-06 00:40:06 -0300

    Inclusão da funcionalidade do botão Pesquisar no HTML; Criação de função e documentação do código em app.js.

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-/* console.log(dados[0].descricao); */
-
-let section = document.getElementById("resultados-pesquisa");
-console.log(section);
-
-let resultados = ""
-
-for (let dado of dados){
-    resultados += `
+function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Constrói o HTML para cada resultado, formatando os dados dinamicamente
+      resultados += `
         <div class="item-resultado">
-            <h2>
-                <a href="#" target="_blank">${dado.titulo}</a>
-            </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href=${dado.link} target="_blank">Mais informações</a>
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Mais informações</a>
         </div>
-    `
-};
-
-section.innerHTML = resultados
+      `;
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
