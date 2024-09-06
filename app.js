edc3f3d7998475d2993f6d1d433f8a42923f6cd6
thiagoafcf <thiago.afcf@gmail.com>
2024-09-06 00:10:43 -0300

    Criação do laço for em JS para exibir os dados no HTML

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(dados[0].descricao);
+/* console.log(dados[0].descricao); */
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+for (let dado of dados){
+    section.innerHTML += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+    `
+}
